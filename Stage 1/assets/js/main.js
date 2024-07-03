7d3d24b07ae9ec293c70c2fdeafe0ee2e7b7830d
tobiloba <tobiosoba777@gmail.com>
2024-07-03 15:12:41 -0700

    Updated

--- Stage 1/assets/js/main.js
+++ Stage 1/assets/js/main.js
@@ -23,3 +23,26 @@ darkMode.addEventListener("click", () => {
         darkModeIcon.classList.replace('bx-sun', 'bx-moon')
     }
 })
+
+
+
+function updateTime() {
+    const dayElement = document.querySelector('#currentTime .day');
+    const timeElement = document.querySelector('#currentTime .time');
+    const now = new Date();
+
+    // Get the day of the week
+    const dayOptions = { weekday: 'long' };
+    const formattedDay = now.toLocaleDateString('en-US', dayOptions);
+
+    // Get the time in the desired format
+    const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    const formattedTime = now.toLocaleTimeString('en-US', timeOptions);
+
+    // Update the content of the elements
+    dayElement.innerText = formattedDay;
+    timeElement.innerText = formattedTime;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
